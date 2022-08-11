3b25b5b5769da2cbfba1dd5f690891e85ee7bb51
BlueBanana450 <62355159+BlueBanana450@users.noreply.github.com>
2022-08-11 15:13:57 -0500

    Add files via upload

---
+++ herokuflasktest.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, abort
+
+server = Flask(__name__)
+
+@server.route('/')
+def index():
+    return server.send_static_file('index.html')
+
+if __name__ == '__main__':
+    server.run(debug=False, host="0.0.0.0", port="5000")
