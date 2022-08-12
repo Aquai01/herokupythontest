e084df117a7c07b1c256a8b80ab67490e66abacc
BlueBanana450 <62355159+BlueBanana450@users.noreply.github.com>
2022-08-11 19:23:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def index():
     return server.send_static_file('index.html')
 
 if __name__ == '__main__':
-    server.run(debug=False, host="0.0.0.0", port="5000")
+    server.run(debug=False, host="0.0.0.0", port="8080")
